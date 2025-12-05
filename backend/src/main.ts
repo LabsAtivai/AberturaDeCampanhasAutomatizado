b5907918c9433b09c2aaecd803f534d47fba73f9
LabsAtivai <150172053+LabsAtivai@users.noreply.github.com>
2025-12-05 12:09:08 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // deixa o Vue falar com o Nest
+  app.enableCors({
+  origin: [
+    'https://aberturas.labsativa.com.br'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}); // deixa o Vue falar com o Nest
 
   await app.listen(3000); // 👈 volta pra 3000
 }
 bootstrap();
+
